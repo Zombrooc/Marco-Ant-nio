c303f5d78b2caf625b9f66afdbe072e736f12927
Elian Valdez <elian.valdez09@gmail.com>
2023-03-05 12:05:25 -0300

    first commit

---
+++ Hello World.py
@@ -0,0 +1,49 @@
+# print('Hello World!') Exibi informações na tela
+
+print(" Marco Antônio ")
+
+# TIPOS DE DADOS
+
+# STRINGS - Textos "Hello World"
+# Integer (INTs) = Números Inteiros - 7
+# Float (Floating Points) - Números com Virgula 3.14159
+# Boolean (Bool) - True || False
+# Array - Listas
+# Tuplas - Lista não mutaveis
+# Dicionarios - JSON
+# Objetc - class
+# complex - Numeros Complexos
+
+x = 7  # Interger
+y = "7"  # String
+
+print(7 + "7")
+
+# OPeradores Artimeticos
+
+# Soma = + (Adição de Número)
+# Subtração = - (Subtração de numeros)
+# Multiplicação = * (Soma sequencial de mesmos número)
+# Divisão = / (Substração sequencial de mesmo  nuemro)
+# Divisão Inteira = //
+# Potencia = **
+# Módulo = % (Retorna o resto de uma divisão)
+
+
+# Expressoções Matemáticas - Ordem de Execução de Calculos
+# {[()]} = Somente parenteses são permitidos as linguagens de programação(Quando se trata de calculos)
+
+# ** - Potencia sempre antes de qualquer outro OPeradores
+
+# * | \
+
+# + | -
+
+# EXEMPLO - CALCULO DE MÉDIA
+
+# Maneira Incorreta
+Média = n1 + n2 + n3 + n4 / 4
+
+# Manera correta
+
+media = (n1 + n2 + n3 + n4)/4
---
+++ URI Online Judge/1003.py
@@ -0,0 +1,5 @@
+A = int(input("Digite um numero"))
+B = int(input("Digite outro numero"))
+
+Soma = A + B
+print(f"Soma={Soma}")
